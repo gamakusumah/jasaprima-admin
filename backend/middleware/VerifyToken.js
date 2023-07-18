b66f0a6509e8ef22b61db30c2958de32436b08e8
gamakusumah <109577032+gamakusumah@users.noreply.github.com>
2023-07-18 16:25:19 +0700

    Update VerifyToken.js

--- backend/middleware/VerifyToken.js
+++ backend/middleware/VerifyToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
+  const authHeader = req.headers["Authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token === null) return res.status(401);
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
