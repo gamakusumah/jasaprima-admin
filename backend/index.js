bda69042605d9da3f2e81e92efce2f1da7a2924f
Gama Kusumah <gamakusumah@gmail.com>
2023-07-18 13:40:30 +0700

    change database and origin url, create vercel config

--- backend/index.js
+++ backend/index.js
@@ -13,16 +13,25 @@ import Kendaraan from "./routes/KendaraanRoute.js";
 
 dotenv.config();
 const app = express();
-mongoose.connect("mongodb://127.0.0.1/jasaprima_db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://gama:l9wcum03029ZM9oA@cluster0.sckbglc.mongodb.net/jasaprima_db?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Database Connected"));
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://jasaprima-admin.vercel.app",
+    methods: ["POST", "GET"],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(AdminRoute);
@@ -32,5 +41,11 @@ app.use(LayananRoute);
 app.use(TransaksiRoute);
 app.use(AreaRoute);
 app.use(Kendaraan);
+app.get(
+  ("/",
+  (req, res) => {
+    res.json("Welcome");
+  })
+);
 
 app.listen(5000, () => console.log("Server up and running..."));
