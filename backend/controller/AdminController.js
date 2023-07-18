d49e20fb6418bc345f50bca03e4fa95f990867d2
Gama Kusumah <gamakusumah@gmail.com>
2023-07-18 14:15:31 +0700

    change error msg

--- backend/controller/AdminController.js
+++ backend/controller/AdminController.js
@@ -107,7 +107,11 @@ export const Masuk = async (req, res) => {
     });
     res.json({ accessToken });
   } catch (error) {
-    res.status(404).json({ msg: "Akun tidak terdaftar!" });
+    res
+      .status(404)
+      .json({
+        msg: `Akun tidak terdaftar!${(req.body.email, req.body.password)} `,
+      });
   }
 };
 
